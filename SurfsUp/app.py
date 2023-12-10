463550dde6b9a06e85b2b5f942e8a9a8162fda0e
Jonah Kopp <jonah.kopp123@gmail.com>
2023-12-10 11:35:48 -0700

    flask app work

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,30 +1,95 @@
 # Import the dependencies.
-
-
+import numpy as np
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from flask import Flask, jsonify
+import datetime as dt
+import pandas as pd
 
 #################################################
 # Database Setup
 #################################################
-
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
-
+Base = automap_base()
 # reflect the tables
-
+Base.prepare(autoload_with=engine)
 
 # Save references to each table
-
-
-# Create our session (link) from Python to the DB
-
+measurement = Base.classes.measurement
+station = Base.classes.station
 
 #################################################
 # Flask Setup
 #################################################
-
-
+app = Flask(__name__)
 
 
 #################################################
 # Flask Routes
 #################################################
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/<start>/<end><br/>"
+    )
+
+
+@app.route("/api/v1.0/precipitation")
+def names():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    max_date = dt.datetime(2017,8,23)
+    min_date = max_date - dt.timedelta(days=366)
+    results_df = pd.DataFrame(session.query(measurement.date,measurement.prcp).filter(measurement.date >= min_date, measurement.date <= max_date).all())
+
+    session.close()
+
+    results_dict = {results_df['date'][i]:results_df['precipitation'][i] for i in range(len(results_df['date']))}
+
+    return jsonify(results_dict)
+
+
+@app.route("/api/v1.0/stations")
+def passengers():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    return jsonify()
+
+
+@app.route("/api/v1.0/tobs")
+def passengers():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    return jsonify()
+
+
+@app.route("/api/v1.0/<start>")
+def passengers():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    return jsonify()
+
+
+@app.route("/api/v1.0/<start>/<end>")
+def passengers():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    return jsonify()
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
